109509983ed2c4f403e5a90ab9ec0e4d73795a0d
Brandon Fuller <BFuller@codecranium.com>
2020-01-18 00:22:31 -0500

    Changed the repsonse type to be explicitly JSON

--- controllers/IncrementController.js
+++ controllers/IncrementController.js
@@ -17,21 +17,20 @@ app.post(controllerPath, (request, response) => {
     if (request.body && request.body.id && request.body.message) {  // Validate that the incoming payload contains: [id] and [message]... if not then respond with invalid message and coordinating error.
         if (counter.consumedValues.some(id => id == request.body.id)) {
             // already accepted, do not increment with the submitted value again.
-            response.status(500).send(JSON.stringify({error: 'Invalid value for [id].  Only unique [id] value is acceptable.'}))
+            response.status(500).json({error: 'Invalid value for [id].  Only unique [id] value is acceptable.'})
         }
         else {
             counter.consumedValues.push(request.body.id);
             const reqCount = request.body.message.split(' ').length;
             counter.currentIncrementValue += reqCount;
-            response.send(JSON.stringify({count: counter.currentIncrementValue}));
-            
+            response.json({count: counter.currentIncrementValue});
         }
     }
     
-    response.status(405).send(JSON.stringify({error: 'Values presented in the payload do not match the expected request structure.  The JSON request should fit the following structure:   { "id": "123", "message": "hello world" }'}));
+    response.status(405).json({error: 'Values presented in the payload do not match the expected request structure.  The JSON request should fit the following structure:   { "id": "123", "message": "hello world" }'});
 })
 
 // Get current count state
 app.get(controllerPath, (request, response) => {
-    response.send(counter);
+    response.json(counter);
 })
